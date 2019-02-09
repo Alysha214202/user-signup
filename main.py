806769a0b014131a9d50c39cbe75f47c192ed547
Alysha O'Hara <Alysha.Griesemer@gmail.com>
2019-02-08 20:52:12 -0600

    Added functions to trigger errors

--- main.py
+++ main.py
@@ -16,13 +16,39 @@ def user_signup_form():
 
 #the user leaves a field empty
 
+def empty_field(x):
+    if x:
+        return True
+    else:
+        return False
+
 #the name/pass are not valid (<3,>20)
 
-#pass & confirmation don't match
+def right_length(x):
+    if len(x) > 3 and len(x) < 20:
+        return True
+    else:
+        return False
 
 #not a valid email (single @, single ., no space, >3, <20 )
 
+def valid_email(x):
+    if x.count('@') = 1:
+        return True
+    else:
+        return False 
+
+def single_period(x):
+    if x.count('.') = 1:
+        return True
+    else:
+        return False
+
+#process form
+
 #feedback message next to field it refers to
+
+#pass & confirmation don't match
 
 #preserve what user wrote for username and email
 
